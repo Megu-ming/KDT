98343c27cfbd8dc6ab2e0cc1b686234aa334e3b3
DESKTOP-6NNKS6R\USER <dudwogkrrmq@gmail.com>
2024-03-18 14:48:06 +0900

    Setup.bat 설정

--- Setup.bat
+++ Setup.bat
@@ -1,5 +1,12 @@
 @echo off
-echo Start Setup.batch
+echo Start Setup.bat
+REM REM은 주석을, echo.은 enter를 의미한다.
+echo.
+echo [vcpkg task]
+call .\vcpkg\bootstrap.vcpkg.bat
+.\vcpkg\vcpkg install rapidjson:x64-windows
+.\vcpkg\vcpkg integrate install
 
+echo.
 echo [done]
 pause
