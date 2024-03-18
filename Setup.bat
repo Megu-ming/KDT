8022c2e39b3ad00bbe7d0360f7b4ef6f14553717
DESKTOP-6NNKS6R\USER <dudwogkrrmq@gmail.com>
2024-03-18 14:53:10 +0900

    Setup.bat 오타수정

--- Setup.bat
+++ Setup.bat
@@ -3,7 +3,7 @@ echo Start Setup.bat
 REM REM은 주석을, echo.은 enter를 의미한다.
 echo.
 echo [vcpkg task]
-call .\vcpkg\bootstrap.vcpkg.bat
+call .\vcpkg\bootstrap-vcpkg.bat
 .\vcpkg\vcpkg install rapidjson:x64-windows
 .\vcpkg\vcpkg integrate install
 
